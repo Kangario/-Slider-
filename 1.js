6cb39896db3209634d159d5aa426e093a964b82c
Kangario <68245183+Kangario@users.noreply.github.com>
2020-08-28 19:58:41 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -6,12 +6,14 @@ let li = all.querySelector ('li');
 but1.onclick = function (){  
     
     position += l;
+     position = Math.min(position,[0])
      ul.style.marginLeft = position + '%';
 
 }
 but2.onclick = function(){  
     
     position -= l;
+    position=Math.max(position, -l * 3)
      ul.style.marginLeft = position + '%';  
   
 }
